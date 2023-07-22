ef57f5baa5603cea0673cfb237ac5c4f0a181526
YasinAflatoon <Yasinaflatoonzade@gmail.com>
2023-07-22 19:21:21 +0330

    redesigned the board for showing highscore.

--- scoreboard.py
+++ scoreboard.py
@@ -2,23 +2,30 @@ from turtle import Turtle
 
 
 class ScoreBoard(Turtle):
+
     def __init__(self):
         super().__init__()
         self.hideturtle()
         self.penup()
-        self.goto(-280, 260)
+        self.goto(-280, 250)
         self.level = 0
+        with open("data.txt") as data_file:
+            self.high_score = int(data_file.read())
         self.level_up()
 
     def level_up(self):
         self.clear()
         self.level += 1
         self.write(
-            arg=f"level: {self.level}",
+            arg=f"Level: {self.level}\nHighscore: {self.high_score}",
             move=False,
             align="left",
             font=("Consolas", 15, "normal")
         )
+        if self.level > self.high_score:
+            with open("data.txt", "w") as data_file:
+                self.high_score = self.level
+                data_file.write(str(self.high_score))
 
     def game_over(self):
         self.home()
